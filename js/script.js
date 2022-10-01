daa3471268d86bd84bea59e4d942455cbbd916cd
AdiDEF004 <ismanaliuuluadilet@gmail.com>
2022-10-01 19:53:57 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector(".about-btn button")
+btn.addEventListener("click",()=> {
+  document.querySelector(".about-modal").style.display = "block"
+})
+
+let close = document.querySelector(".about-modal-bg-square div")
+close.addEventListener("click",() => {
+    document.querySelector('.about-modal').style.display = "none"
+})
+
+
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 25,
+        stretch: 0,
+        depth: 50,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
